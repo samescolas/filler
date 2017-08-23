57373da781b1289f7a65dff2331b142b4096ceae
Sam Escolas <samescolas@gmail.com>
2017-08-23 18:56:00 -0400

    Adding ft_read_baord

---
+++ srcs/ft_read_board.c
@@ -0,0 +1,19 @@
+#include "filler.h"
+
+int	ft_read_board(char ***board, size_t rows)
+{
+	char	*input;
+	int		i;
+
+	if (ft_flush_lines(2) < 0)
+		return (-1);
+	i = -1;
+	while (++i < rows - 1)
+	{
+		if (get_next_line(STDIN_FILENO, &input) < 0)
+			return (-1);
+		(*board)[i] = &input[4];
+	}
+	return (0);
+}
+
