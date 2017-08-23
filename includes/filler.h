e721657034376fefbfde50836577b2ef1e6d0386
Sam Escolas <samescolas@gmail.com>
2017-08-23 18:36:31 -0400

    Adding header file.

--- includes/filler.h
+++ includes/filler.h
@@ -1,25 +1,28 @@
 #ifndef FILLER_H
 # define FILLER_H
 
+# include "../libft/libft.h"
+
 typedef struct	s_coord
 {
-	size_t	x;
-	size_t	y;
-}		t_coord;
+	size_t		x;
+	size_t		y;
+}				t_coord;
 
 typedef struct	s_piece
 {
-	t_coord	dim;
-	t_coord	pos;
-}		t_piece;
+	t_coord		dim;
+	t_coord		pos;
+	char		**str;
+}				t_piece;
 
 typedef struct	s_game
 {
-	char	player;
-	char	marker;
-	t_coord	dim;
-	char	**board;
-	t_piece	*piece;
-}		t_game;
+	char		player;
+	char		marker;
+	t_coord		dim;
+	char		**board;
+	t_piece		*piece;
+}				t_game;
 
 #endif
