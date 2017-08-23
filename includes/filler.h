c04a9b0785e5d7e53bab5639bb99c268858ec714
Sam Escolas <samescolas@gmail.com>
2017-08-23 15:17:22 -0400

    Adding thugs

--- includes/filler.h
+++ includes/filler.h
@@ -9,15 +9,15 @@ typedef struct	s_coord
 
 typedef struct	s_piece
 {
-	t_coord	*dim;
-	t_coord	*pos;
+	t_coord	dim;
+	t_coord	pos;
 }		t_piece;
 
 typedef struct	s_game
 {
 	char	player;
 	char	marker;
-	t_coord	*dim;
+	t_coord	dim;
 	char	**board;
 	t_piece	*piece;
 }		t_game;
--- srcs/filler.c
+++ srcs/filler.c
@@ -1,8 +1,17 @@
 #include "filler.h"
 
-static int	ft_new_game(t_game *game)
+static int	ft_new_game(t_game *game, t_piece *piece)
 {
-	
+	piece->dim->x = 0;
+	piece->dim->y = 0;
+	piece->pos->x = 0;
+	piece->pos->y = 0;
+	game->player = ft_get_player_number();
+	game->marker = (game->player == 1 ? 'o' : 'x');
+	game->dim->x = 0;
+	game->dim->y = 0;
+	game->board = (void *)0;
+	game->piece = piece;
 }
 
 int	main(void)
